9b45276e7561305b39d40080db6d3818168a3d40
Lantharen <lantharen5@gmail.com>
2022-10-07 13:12:19 +0300

    crud

---
+++ delete.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$id = $_GET['id'] ?? null;
+
+$id = (int)$id;
+
+
+$connect = mysqli_connect('127.0.0.1', 'root', '', 'site');
+
+$query = "SELECT * FROM `names` WHERE `id` = '$id'";
+//echo $query;
+$result = mysqli_query($connect, $query) or die (mysqli_error($connect));
+if (mysqli_num_rows($result) > 0) {
+    echo "Строка найдена. Удаляем...";
+    $query = "DELETE FROM `names` WHERE `id`  = '$id'";
+    mysqli_query($connect, $query) or die (mysqli_error($connect));
+    $query = "SELECT * FROM `names` WHERE `id` = '$id'";
+    $result = mysqli_query($connect, $query) or die (mysqli_error($connect));
+    if (mysqli_num_rows($result) == 0) {
+        echo "</br>Строка удалена.";
+    }
+}
+
+
+echo "</br></br><a href='crud.php'>Вернуться</a>";
+
+
+mysqli_close($connect);
--- update.php
+++ update.php
@@ -1,30 +1,27 @@
 <?php
-$id = "";
-$firstname = "";
-$lastname = "";
 
-if (isset($_POST['id'])) {
-    $id = $_POST['id'];
-}
-if (isset($_POST['firstname'])) {
-    $firstname = $_POST['firstname'];
-}
-if (isset($_POST['lastname'])) {
-    $lastname = ($_POST['lastname']);
+$id        = $_POST['id'] ?? null;
+$firstName = $_POST['firstname'] ?? null;
+$lastName  = $_POST['lastname'] ?? null;
+
+if (null === $id || null === $firstName || null === $lastName) {
+    die('Missing data!');
 }
 
+$id        = (int) $id;
+$firstName = htmlspecialchars($firstName);
+$lastName  = htmlspecialchars($lastName);
 
-$connect =mysqli_connect('127.0.0.1', 'root', '','site');
-$query = 'UPDATE names SET firstname = "'. $firstname .'", lastname = "'. $lastname .'" WHERE id = '.$id;
-$result = mysqli_query($connect,$query);
+$connect = mysqli_connect('127.0.0.1', 'root', '', 'site');
+$query   = 'UPDATE `names` SET `firstname` = "'.$firstName.'", `lastname` = "'.$lastName.'" WHERE `id` = '.$id;
+
+$result  = mysqli_query($connect, $query);
+
+if (false === $result) {
+    die('Error!');
+}
+
 mysqli_close($connect);
 
-
-echo "
-<script type=\"text/javascript\">
-    window.location.href = \"crud.php\";
-</script>
-";
-
-
-?>
+header('Location: crud.php');
+die();
